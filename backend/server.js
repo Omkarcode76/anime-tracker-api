25ed92d6d9d2c7c711c9d9f010ed7ea7db7d534d
omkar <omkar076@icloud.com>
2026-03-23 18:09:53 +0530

    Improve server.js code

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,28 @@
 import express from "express";
-import mongoose from "mongoose";
+import connectDB from "./db.js";
 import dotenv from "dotenv";
 import animeRouter from "./routes/animeRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import authMiddleware from "./middleware/userAuthMiddleware.js";
-import cors from "cors"
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const port = 3000;
-mongoose.connect(process.env.MONGO_URI);
 app.use(express.json());
-app.use(cors())
-app.use("/anime",authMiddleware, animeRouter);
-app.use("/user", userRouter,);
+app.use(cors());
+app.use("/anime", authMiddleware, animeRouter);
+app.use("/user", userRouter);
 
-app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error("failed to start the server", error.message);
+    process.exit(1);
+  }
+};
+startServer();
